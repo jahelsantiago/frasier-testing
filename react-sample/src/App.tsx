e62ccec8e8c3ef148566cc3778b6b6276a77a420
Jahel <jleonro@unal.edu.co>
2023-09-10 21:40:25 -0500

    This adds a deletes some code

--- react-sample/src/App.tsx
+++ react-sample/src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
@@ -18,9 +17,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -28,6 +24,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>This is a new addition</p>
     </>
   )
 }
